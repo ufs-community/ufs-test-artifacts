52e9728010d9204f19029eec0e7c6313a9a8c8ff
Mark A Potts <Mark.Potts@noaa.gov>
2021-07-15 16:43:58 +0000

    Starting with fresh branch

---
+++ python_scripts/archive_results.py
@@ -0,0 +1,110 @@
+import os
+import yaml
+import subprocess
+import argparse
+import datetime
+import sys
+import time
+import glob
+import re
+import pathlib
+from scheduler import scheduler
+from noscheduler import NoScheduler
+from pbs import pbs
+from slurm import slurm
+
+
+
+
+class ArchiveResults:
+  def __init__(self,args):
+
+    self.jobid = args['jobid']
+
+    with open(args['yaml']) as file:
+      yaml_list = yaml.load(file, Loader=yaml.FullLoader)
+      if self.jobid == "None":
+        self.jobid = yaml_list['jobid']
+      self.rundir = yaml_list['collectiondir']
+      self.machine_name = yaml_list['hostname']
+      scheduler = yaml_list['scheduler']
+      print("HEY, scheduler is {}".format(scheduler))
+      self.artifacts_root = yaml_list['artifactsdir']
+      if(yaml_list['artifactname']):
+        self.artifactname=yaml_list['artifactname'];
+      else:
+        self.artifactname="default"
+
+
+    self.root_path = pathlib.Path(__file__).parent.absolute()
+    if(scheduler == "pbs"):
+      self.scheduler=pbs("pbs")
+    elif(scheduler == "slurm"):
+      self.scheduler=slurm("slurm")
+    elif(scheduler == "None" ):
+      print("creating no scheduler")
+      self.scheduler=NoScheduler("None")
+    else:
+      print("bad scheduler type")
+      return
+    if(args['dryrun'] == "True"):
+      self.dryrun = True
+    else:
+      self.dryrun = False
+    
+    print("dryrun is -- {}".format(self.dryrun))
+    start_time = time.time()
+    seconds = 144000
+    while True:
+      current_time = time.time()
+      elapsed_time = current_time - start_time
+      job_done = self.scheduler.checkqueue(self.jobid)
+      if(job_done):
+        oe_filelist = [];
+        for key in yaml_list['collectedfiles']:
+          regex = yaml_list['collectedfiles'][key]
+          print("HEY, regex is {}/{}".format(self.rundir,regex))
+          oe_filelist.extend(glob.glob('{}/{}'.format(self.rundir,regex)))
+        print("filelist is {}".format(oe_filelist))
+        print("oe list is {}\n".format(oe_filelist))
+        self.copy_artifacts(oe_filelist)
+        break
+      time.sleep(30)
+
+      if elapsed_time > seconds:
+         print("Finished iterating in: " + str(int(elapsed_time))  + " seconds")
+         break
+
+  def runcmd(self,cmd):
+    if(self.dryrun == True):
+       print("would have executed {}".format(cmd))
+    else:
+       os.system(cmd)
+
+  def copy_artifacts(self,oe_filelist):
+  
+    cwd = os.getcwd()
+    os.chdir(self.artifacts_root)
+    if(oe_filelist == []):
+      return
+    mkdir_cmd = "mkdir -p {}".format(self.machine_name)
+    self.runcmd(mkdir_cmd)
+    for cfile in oe_filelist:
+      cp_cmd = 'cp {} ./{}'.format(cfile,self.machine_name)
+      print("cp command is {}".format(cp_cmd))
+      self.runcmd(cp_cmd)
+
+    git_cmd = "git add *;git commit -a -m\'update for {} on {}';git pull origin main --no-edit;git push origin main".format(self.artifactname,self.machine_name)
+    self.runcmd(git_cmd)
+    return
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='ESMF nightly build/test system')
+  parser.add_argument('-j','--jobid', help='directory where builds will be mad #', required=False,default="None")
+  parser.add_argument('-y','--yaml', help='Yaml file defining builds and testing parameters', required=True)
+  parser.add_argument('-d','--dryrun', help='directory where artifacts will be placed', required=False,default=False)
+  args = vars(parser.parse_args())
+   
+  archiver = ArchiveResults(args)
+    
---
+++ python_scripts/plotdata.py
@@ -0,0 +1,135 @@
+import os
+import yaml
+import subprocess
+import argparse
+import datetime
+import sys
+import time
+import glob
+import re
+import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+class PlotData:
+  def __init__(self,args):
+
+    self.machinename= args['machine']
+
+    if(args['dryrun'] == "True"):
+      self.dryrun = True
+    else:
+      self.dryrun = False
+    timing = {} 
+    print("dryrun is -- {}".format(self.dryrun))
+    git_cmd = "git log --pretty=oneline | grep {}".format(self.machinename)
+    output = self.runcmd(git_cmd)
+    components = ['ATM','OCN','ICE','WAV']
+    for line in output:
+      words = line.split(" ")
+      githash = words[0]
+      versioncmd = "git show {}:{}/PET000.ESMF_LogFile | grep \"ESMF Version\"".format(githash,self.machinename)
+      output = self.runcmd(versioncmd)
+      words = output[0].split(": ")
+      ESMFversion = words[1].split("'")[0]
+      comptiming = {}
+      for comp in components:
+        cmd = "git show {}:{}/ESMF_Profile.summary | grep \"\[{}\] RunPhase1\"".format(githash,self.machinename,comp)
+        output = self.runcmd(cmd)
+        if(output != None):
+          words = output[0].split()
+          print("{} {} and {}".format(ESMFversion,githash,words))
+#         print("{} {}".format(ESMFversion,words[4]))
+          comptiming[comp] = words[7]
+        else:
+          comptiming[comp] = 0.0
+# sum up the mediator timing as well
+      cmd = "git show {}:{}/ESMF_Profile.summary | grep \"\[MED\]\" | awk \-F \"\[\[:space:\]\]\[\[:space:\]\]+\" \"{{print \$8}}\" | paste -sd+ - | bc".format(githash,self.machinename)
+      output = self.runcmd(cmd)
+      if(output == []):
+        comptiming['MED'] = 0.0
+      else:
+        comptiming['MED'] = output[0]
+      if(comptiming != {}):
+        cmd = "git show {}:{}/out | grep \"Total runtime\"".format(githash,self.machinename)
+        output = self.runcmd(cmd)
+        if(output != None ):
+          runtime = output[0].split()[3]
+          comptiming['runtime']=runtime
+        if ESMFversion in timing.keys():
+          pass
+        else:
+          timing[ESMFversion] = comptiming
+    labels = sorted(timing.keys())
+    print(labels)
+    labels = [w.replace('ESMF_', '') for w in labels]
+    labels = [w.replace('beta_snapshot_', '') for w in labels]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.175  # the width of the bars
+    atm = np.zeros(len(timing))
+    ocn = np.zeros(len(timing))
+    ice = np.zeros(len(timing))
+    wav = np.zeros(len(timing))
+    med = np.zeros(len(timing))
+    i = 0
+    for key in sorted (timing.keys()):
+      atm[i] = timing[key]['ATM']
+      ocn[i] = timing[key]['OCN']
+      ice[i] = timing[key]['ICE']
+      wav[i] = timing[key]['WAV']
+      med[i] = timing[key]['MED']
+      i = i +1
+
+    fig, ax = plt.subplots(figsize=(12,4))
+    rects1 = ax.bar(x - width*1.5, atm, width, label='ATM')
+    rects2 = ax.bar(x - width*0.5, ocn, width, label='OCN')
+    rects3 = ax.bar(x + width*0.5, ice, width, label='ICE')
+    rects4 = ax.bar(x + width*1.5, wav, width, label='WAV')
+    rects5 = ax.bar(x + width*2.5, med, width, label='MED')
+
+    ax.set_ylabel('Time (seconds)')
+    ax.set_title('Component (MAX) Timing by ESMF snapshot on {}'.format(self.machinename))
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    ax.bar_label(rects3, padding=3)
+    ax.bar_label(rects4, padding=3)
+    ax.bar_label(rects5, padding=3)
+
+    fig.tight_layout()
+
+    plt.savefig('{}-comp-timing.png'.format(self.machinename))
+    plt.show()
+    print(atm)
+    print(labels)
+    return
+
+
+  def runcmd(self,cmd):
+    if(self.dryrun == True):
+       print("would have executed {}".format(cmd))
+       output = None
+    else:
+      try:
+        output = subprocess.check_output(cmd,shell=True).strip().decode('utf-8').splitlines()
+      except:
+        output = None
+    return output
+
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Plot changes over time in UFS coupled benchmarks')
+  parser.add_argument('-m','--machine', help='name of machine used for plots', required=True)
+  parser.add_argument('-d','--dryrun', help='dryrun (optional)', required=False,default=False)
+  args = vars(parser.parse_args())
+   
+  plotter = PlotData(args)
+    
