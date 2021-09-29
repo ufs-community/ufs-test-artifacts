e787843d0c40e0120745728a0a28fbce780f38e6
Mark A Potts <Mark.Potts@noaa.gov>
2021-09-29 16:50:10 +0000

    re-factored build of s2sw to use rt.sh

--- misc_scripts/check_git.pl
+++ misc_scripts/check_git.pl
@@ -15,14 +15,8 @@ foreach(@gitlines) {
     system("sed -i 's/ESMFTAG/$tag/g' config/stack_esmf.yaml");
     system("sed -i 's/ESMFDEBUG/NO/g' config/stack_esmf.yaml");
     system("./build_stack.sh -p /scratch1/NCEPDEV/da/Mark.Potts/sandbox/hpc-modules -c config/config_hera.sh -y config/stack_esmf.yaml -m");
-#   system("cp config/stack_esmf_template.yaml config/stack_esmf.yaml");
-#   system("sed -i 's/ESMFTAG/$tag/g' config/stack_esmf.yaml");
-#   system("sed -i 's/ESMFDEBUG/YES/g' config/stack_esmf.yaml");
-#   system("./build_stack.sh -p /scratch1/NCEPDEV/da/Mark.Potts/sandbox/hpc-modules -c config/config_hera.sh -y config/stack_esmf.yaml -m");
-    system("cp /scratch1/NCEPDEV/da/Mark.Potts/sandbox/ufs-weather-model/modulefiles/ufs_common.template /scratch1/NCEPDEV/da/Mark.Potts/sandbox/ufs-weather-model/modulefiles/ufs_common");
-    system("cp /scratch1/NCEPDEV/da/Mark.Potts/sandbox/ufs-weather-model/modulefiles/ufs_hera.intel.template /scratch1/NCEPDEV/da/Mark.Potts/sandbox/ufs-weather-model/modulefiles/ufs_hera.intel");
-    system("sed -i 's/ESMFTAG/$tag/g' /scratch1/NCEPDEV/da/Mark.Potts/sandbox/ufs-weather-model/modulefiles/ufs_common");
-    system("/home/Mark.Potts/build_s2s.sh");
+
+    system("/home/Mark.Potts/rebuild_s2s.sh $tag");
   }
 }
 
