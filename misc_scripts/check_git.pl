848119b273a5d9bc1b78acd61945950d943f9c49
Mark Potts <mpotts@redlineperf.com>
2021-10-13 07:41:26 -0500

    changed to build esmf standalone

--- misc_scripts/check_git.pl
+++ misc_scripts/check_git.pl
@@ -6,7 +6,7 @@ $hpc_modules_dir = @ARGV[2];
 $ufs_artifacts_dir = @ARGV[3];
 chdir $esmf_dir;
 @gitlines = split(/\n/,`git remote update 2>&1`);
-#@gitlines[0] = " * [new tag]         ESMF_8_2_0_beta_snapshot_14 -> ESMF_8_1_1_beta_snapshot_02";
+#@gitlines[0] = " * [new tag]         ESMF_8_2_0_beta_snapshot_19 -> ESMF_8_1_1_beta_snapshot_19";
 chdir $hpc_stack_dir;
 foreach(@gitlines) {
   print("line is $_\n");
@@ -15,12 +15,9 @@ foreach(@gitlines) {
     print("words are @words\n");
     $tag = @words[4];
     $tag =~ s/ESMF_//g;
-    system("cp config/stack_esmf_template.yaml config/stack_esmf.yaml");
-    system("sed -i 's/ESMFTAG/$tag/g' config/stack_esmf.yaml");
-    system("sed -i 's/ESMFDEBUG/NO/g' config/stack_esmf.yaml");
-    system("./build_stack.sh -p $hpc_modules_dir -c config/config_hera.sh -y config/stack_esmf.yaml -m");
-
-    system("$ufs_artifacts_dir/rebuild_s2s.sh $tag");
+    system("cp stack/stack_esmf_template.yaml stack/stack_noaa.yaml");
+    system("sed -i 's/ESMFTAG/$tag/g' stack/stack_noaa.yaml");
+    system("./build_standalone_esmf.sh");
   }
 }
 
